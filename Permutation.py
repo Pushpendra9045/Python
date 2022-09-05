9a1774fb512248e4ddbb730610da2286d64e6f8b
Pushpendra202111 <84605640+Pushpendra9045@users.noreply.github.com>
2022-09-05 09:05:34 +0530

    Delete Permutation.py

--- Permutation.py
+++
@@ -1,9 +0,0 @@
-word="KAI"
-result=[]
-for i in range(len(word)):
-    for j in range(len(word)):
-        for k in range(len(word)):
-            if i==j or j==k or i==k:
-                continue
-            result.append(word[i]+word[j]+word[k])
-print(result)
